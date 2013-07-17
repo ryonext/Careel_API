29e7bc5738140c604137a460478310190fd32184
ryonext <ryonext.s@gmail.com>
2013-07-17 21:09:14 +0900

    create rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Careel::Application.config.secret_key_base = '60d713b10e76237b840ad0f358733b8febe02a092fe1807d502d9419fb637a941d535e4b95f2b02c064fab374a1804f0a410fb84996eb192819e5f1a270473cb'
